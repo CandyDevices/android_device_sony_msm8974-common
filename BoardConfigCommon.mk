b996141cb0a7bec27e6e6542eef4cebf6d77e301
Alx31 <alex31.couderc@gmail.com>
2015-06-12 22:18:37 +0200

    nope, keep it this way

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -18,6 +18,9 @@ include device/sony/common/BoardConfigCommon.mk
 TARGET_NO_BOOTLOADER := true
 TARGET_NO_RADIOIMAGE := true
 
+# Kernel properties
+TARGET_KERNEL_SOURCE := kernel/sony/msm8974
+
 # use CAF variants
 BOARD_USES_QCOM_HARDWARE := true
 
